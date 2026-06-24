78229c63c18faa96fdbf6132a0b33e313a1fc9e0
EonEmmanuel <emmanueltresor027@gmail.com>
2026-06-24 11:42:17 +0100

    auth routes and controller

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,10 @@
-import { Router} from "express";
+import { Router } from "express";
+import { authCallBack, getMe } from "../controllers/authController";
+import { protectRoute } from "../middleware/auth";
 
 const router = Router();
+
+router.get("/me", protectRoute, getMe);
+router.post("/callback", authCallBack);
 
 export default router;
